97f9e9d0a16c6560b69595f887aa2e283ac0b044
earauzmorales <e.morales@evision-software.com>
2020-02-10 09:10:45 +0100

    Adds v1.0.0 visual interface and engine for converting Excel to XML resources files

---
+++ ExcelToXMLTool/ExcelToXMLTool/ConversorEngine/ExcelToXMLConverter.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+using Microsoft.Office.Interop.Excel;
+using Application = Microsoft.Office.Interop.Excel.Application;
+
+namespace ExcelToXML.Engine
+{
+    public delegate void PercentageFileProcessedHandler(int porcentageProcessed);
+    public delegate void FileProcessingHandler(string message);
+
+    public class ExcelToXMLConverter
+    {
+        private string _sSelectedExcelFile;
+        private string _sSelectedOutputPath;
+        private Application _ExcelApp;
+        private Workbook _workbook;
+        private Dictionary<string, Worksheet> _sheetsDictionary;
+        private const string KEYCOL = "A";
+        private const string CATEGORYCOL = "B";
+        private const string TYPECOL = "C";
+        private Dictionary<string, string> _langCols;
+        private const string rootNode = "root";
+        private const string resheaderNode = "resheader";
+        private const string dataNode = "data";
+        private const string valueNode = "value";
+        private int _iPorcentageProcessed = 0;
+        private int _iTotalOperations = 0;
+
+        public event PercentageFileProcessedHandler PercentageProcessed;
+        public event FileProcessingHandler FileProcessing;
+
+        public ExcelToXMLConverter(string inputFile, string outputFolder)
+        {
+            _sSelectedExcelFile = inputFile;
+            _sSelectedOutputPath = outputFolder;
+            _ExcelApp = new Application();
+            _workbook = _ExcelApp.Workbooks.Open(_sSelectedExcelFile, 0, true, 5, "", "", true, XlPlatform.xlWindows, "\t", false, false, 0, true);
+            _sheetsDictionary = new Dictionary<string, Worksheet>();
+            _langCols = new Dictionary<string, string>();
+
+            foreach (Worksheet sheet in _workbook.Worksheets)
+            {
+                _sheetsDictionary.Add(sheet.Name, sheet);
+            }
+        }
+
+        public void Convert()
+        {
+            foreach (KeyValuePair<string, Worksheet> kvp in _sheetsDictionary)
+            {
+                ProcessSheet(kvp.Value);
+            }
+        }
+
+        private List<XmlDocument> ProcessSheet(Worksheet sheet)
+        {
+            string rootFileName = sheet.Name;
+            List<XmlDocument> docs = new List<XmlDocument>();
+            Dictionary<string, Range> columns = new Dictionary<string, Range>();
+            Range labelsColumn = null;
+            foreach (Range column in sheet.UsedRange.Columns)
+            {
+                string colAddress = column.Address.Replace("$", "").Split(':')[0].Substring(0, 1);
+                if(!colAddress.Equals(CATEGORYCOL) && !colAddress.Equals(TYPECOL))
+                {
+                    string columnLabel = column.Cells[1, 1].Value2 as string;
+                    if (columnLabel != null)
+                    {
+                        if (!colAddress.Equals(KEYCOL))
+                        {
+                            columnLabel = columnLabel.Split('[')[1].Split(']')[0];
+                            columns.Add(columnLabel, column);
+                        }
+                        else
+                        {
+                            labelsColumn = column;
+                            _iTotalOperations = column.Rows.Count - 1;
+                        }
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<string, Range> column in columns)
+            {
+                string columnName = column.Key;
+                string fileName = Path.Combine(_sSelectedOutputPath, $"{rootFileName}_{columnName}.resx.xml");
+                FileProcessing?.Invoke($"Exporting content to file '{rootFileName}_{columnName}.resx.xml'...");
+                XmlDocument doc = CreateLanguageXML(labelsColumn, column.Value);
+                if (File.Exists(fileName))
+                {
+                    if (MessageBox.Show($"The file '{rootFileName}_{columnName}.resx.xml' already exists in the selected folder.\nDo you want to replace it?",
+                            "File already exists",
+                            MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        doc.Save(fileName);
+                    }
+                }
+                FileProcessing?.Invoke($"File '{rootFileName}_{columnName}.resx.xml' successfully exported");
+            }
+
+            return docs;
+        }
+
+        private XmlDocument CreateLanguageXML(Range labels, Range translation)
+        {
+            XmlDocument document = new XmlDocument();
+            GenerateXmlDocumentHeader(document);
+
+            for (int i = 2; i <= labels.Rows.Count; i++)
+            {
+                object labelObj = labels.Cells[i, 1].Value2;
+                object translationObj = translation.Cells[i, 1].Value2;
+                string labelString = string.Empty;
+                string translationString = string.Empty;
+                if (labelObj != null)
+                {
+                    labelString = labelObj.ToString();
+                }
+                if (translationObj != null)
+                {
+                    translationString = translationObj.ToString();
+                }
+                AddResourceEntry(labelString, translationString, document);
+                _iPorcentageProcessed = 100 * i / _iTotalOperations;
+                if (_iPorcentageProcessed > 101)
+                {
+                    _iPorcentageProcessed = 101;
+                }
+                PercentageProcessed?.Invoke(_iPorcentageProcessed);
+            }
+
+            return document;
+        }
+
+        private void GenerateXmlDocumentHeader(XmlDocument document)
+        {
+            XmlDeclaration xmlDeclaration = document.CreateXmlDeclaration("1.0", "UTF-8", null);
+            document.AppendChild(xmlDeclaration);
+            XmlElement root = document.CreateElement(rootNode);
+            XmlElement resheaderElementMimeType = document.CreateElement(resheaderNode);
+            XmlElement valueElement = document.CreateElement(valueNode);
+            resheaderElementMimeType.SetAttribute("name", "resmimetype");
+            valueElement.InnerText = "text/microsoft-resx";
+            resheaderElementMimeType.AppendChild(valueElement);
+            XmlElement resheaderElementVersion = document.CreateElement(resheaderNode);
+            resheaderElementVersion.SetAttribute("name", "version");
+            valueElement = document.CreateElement(valueNode);
+            valueElement.InnerText = "2.0";
+            resheaderElementVersion.AppendChild(valueElement);
+            XmlElement resheaderElementReader = document.CreateElement(resheaderNode);
+            resheaderElementReader.SetAttribute("name", "reader");
+            valueElement = document.CreateElement(valueNode);
+            valueElement.InnerText = "System.Resources.ResXResourceReader, System.Windows.Forms, Version=4.0.0.0, Culture=neutral,PublicKeyToken = b77a5c561934e089";
+            resheaderElementReader.AppendChild(valueElement);
+            XmlElement resheaderElementWriter = document.CreateElement(resheaderNode);
+            resheaderElementWriter.SetAttribute("name", "reader");
+            valueElement = document.CreateElement(valueNode);
+            valueElement.InnerText = "System.Resources.ResXResourceWriter, System.Windows.Forms, Version=4.0.0.0, Culture=neutral,PublicKeyToken = b77a5c561934e089";
+            resheaderElementWriter.AppendChild(valueElement);
+            root.AppendChild(resheaderElementMimeType);
+            root.AppendChild(resheaderElementVersion);
+            root.AppendChild(resheaderElementReader);
+            root.AppendChild(resheaderElementWriter);
+            document.AppendChild(root);
+        }
+
+        public void AddResourceEntry(string label, string translation, XmlDocument document)
+        {
+            if (translation != string.Empty)
+            {
+                XmlElement dataElement = document.CreateElement(dataNode);
+                XmlElement valueElement = document.CreateElement(valueNode);
+                dataElement.SetAttribute("name", label);
+                valueElement.InnerText = translation;
+                dataElement.AppendChild(valueElement);
+                document.ChildNodes[1].AppendChild(dataElement); 
+            }
+        }
+    }
+}
---
+++ ExcelToXMLTool/ExcelToXMLTool/Presentation/ExcelToXMLMainForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace ExcelToXML.Presentation
+{
+    partial class ExcelToXMLMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._excelFolderPathField = new System.Windows.Forms.TextBox();
+            this._openExcelFileBtn = new System.Windows.Forms.Button();
+            this._openOutputFolderBtn = new System.Windows.Forms.Button();
+            this._xmlFilePathField = new System.Windows.Forms.TextBox();
+            this._convertToXMLBtn = new System.Windows.Forms.Button();
+            this._progressBar = new System.Windows.Forms.ProgressBar();
+            this._outputTextArea = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // _excelFolderPathField
+            // 
+            this._excelFolderPathField.Location = new System.Drawing.Point(27, 36);
+            this._excelFolderPathField.Name = "_excelFolderPathField";
+            this._excelFolderPathField.Size = new System.Drawing.Size(425, 22);
+            this._excelFolderPathField.TabIndex = 0;
+            this._excelFolderPathField.TextChanged += new System.EventHandler(this.OnFilesFilterTextChanged);
+            // 
+            // _openExcelFileBtn
+            // 
+            this._openExcelFileBtn.Location = new System.Drawing.Point(458, 30);
+            this._openExcelFileBtn.Name = "_openExcelFileBtn";
+            this._openExcelFileBtn.Size = new System.Drawing.Size(125, 34);
+            this._openExcelFileBtn.TabIndex = 1;
+            this._openExcelFileBtn.Text = "Search file";
+            this._openExcelFileBtn.UseVisualStyleBackColor = true;
+            this._openExcelFileBtn.Click += new System.EventHandler(this.OnOpenExcelFileClicked);
+            // 
+            // _openOutputFolderBtn
+            // 
+            this._openOutputFolderBtn.Location = new System.Drawing.Point(458, 81);
+            this._openOutputFolderBtn.Name = "_openOutputFolderBtn";
+            this._openOutputFolderBtn.Size = new System.Drawing.Size(125, 34);
+            this._openOutputFolderBtn.TabIndex = 2;
+            this._openOutputFolderBtn.Text = "Open folder";
+            this._openOutputFolderBtn.UseVisualStyleBackColor = true;
+            this._openOutputFolderBtn.Click += new System.EventHandler(this.OnOpenXMLFolderClicked);
+            // 
+            // _xmlFilePathField
+            // 
+            this._xmlFilePathField.Location = new System.Drawing.Point(27, 87);
+            this._xmlFilePathField.Name = "_xmlFilePathField";
+            this._xmlFilePathField.Size = new System.Drawing.Size(425, 22);
+            this._xmlFilePathField.TabIndex = 3;
+            this._xmlFilePathField.TextChanged += new System.EventHandler(this.OnFilesFilterTextChanged);
+            // 
+            // _convertToXMLBtn
+            // 
+            this._convertToXMLBtn.Enabled = false;
+            this._convertToXMLBtn.Location = new System.Drawing.Point(212, 125);
+            this._convertToXMLBtn.Name = "_convertToXMLBtn";
+            this._convertToXMLBtn.Size = new System.Drawing.Size(189, 46);
+            this._convertToXMLBtn.TabIndex = 4;
+            this._convertToXMLBtn.Text = "Convert to XML";
+            this._convertToXMLBtn.UseVisualStyleBackColor = true;
+            this._convertToXMLBtn.Click += new System.EventHandler(this.OnConvertButtonClicked);
+            // 
+            // _progressBar
+            // 
+            this._progressBar.Location = new System.Drawing.Point(27, 177);
+            this._progressBar.Name = "_progressBar";
+            this._progressBar.Size = new System.Drawing.Size(556, 23);
+            this._progressBar.TabIndex = 5;
+            // 
+            // _outputTextArea
+            // 
+            this._outputTextArea.Location = new System.Drawing.Point(27, 219);
+            this._outputTextArea.Multiline = true;
+            this._outputTextArea.Name = "_outputTextArea";
+            this._outputTextArea.ReadOnly = true;
+            this._outputTextArea.Size = new System.Drawing.Size(556, 243);
+            this._outputTextArea.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Resources excel file path:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Output XML files path:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 488);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._outputTextArea);
+            this.Controls.Add(this._progressBar);
+            this.Controls.Add(this._convertToXMLBtn);
+            this.Controls.Add(this._xmlFilePathField);
+            this.Controls.Add(this._openOutputFolderBtn);
+            this.Controls.Add(this._openExcelFileBtn);
+            this.Controls.Add(this._excelFolderPathField);
+            this.Name = "ExcelToXMLMainForm";
+            this.Text = "Excel To XML Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button _openExcelFileBtn;
+        private Button _openOutputFolderBtn;
+        private TextBox _xmlFilePathField;
+        private Button _convertToXMLBtn;
+        private ProgressBar _progressBar;
+        private TextBox _outputTextArea;
+        private Label label1;
+        private Label label2;
+    }
+}
+
